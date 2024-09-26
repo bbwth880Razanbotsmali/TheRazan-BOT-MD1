05cb77e523fb22c61d00c26951871c2eaeb8f846
Ahmad77699900 <bbwth880@gmail.com>
2024-09-26 19:09:52 +0300

    Update دعوه.js

--- plugins/دعوه.js
+++ plugins/دعوه.js
@@ -1,12 +1,12 @@
 
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-if (!text) throw `*اكــتـب الـرقـم الـذي تـريـد ارســال لـه الـدعوه !*\n*مــثـال :\n*${usedPrefix + command}* 201554582851`
+if (!text) throw `*اكــتـب الـرقـم الـذي تـريـد ارســال لـه الـدعوه !*\n*مــثـال :\n*${usedPrefix + command}* 967779151032`
 if (text.includes('+')) throw  `*لا تــضـع هـذه الـعـلـامـه +*`
 if (isNaN(text)) throw '*دخــل الـرقـم بـدـون مـسافـات !*'
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
 
-      await conn.reply(text+'@s.whatsapp.net', `*دعــوه لــدـخول الـجـروب !*\n*الــرابـط :* ${link}`, m, {mentions: [m.sender]})
+      await conn.reply(text+'@s.whatsapp.net', `*دعــوه لدخول الـجـروب !*\n*الــرابـط :* ${link}`, m, {mentions: [m.sender]})
         m.reply(`*تــم ارســال رابــط الـدعـوه !*`) 
 
 }
