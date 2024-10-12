d7ef5eacf3d2d621307aaa50eabe787bff509ad8
Ahmad77699900 <bbwth880@gmail.com>
2024-10-12 18:52:30 +0300

    Update رد3.js

--- plugins/رد3.js
+++ plugins/رد3.js
@@ -4,10 +4,10 @@ let handler = async (m, { conn }) => {
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
     let message = `*قــلــب رزان مـن جـوا 👀⚡*`;
 await conn.sendMessage(m.chat, { react: { text: '🙌', key: m.key } })
-    conn.sendFile(m.chat, 'https://envs.sh/06H.jpg', 'image.jpg', message, m);
+    conn.sendFile(m.chat, 'https://qu.ax/rNkWf.jpg', 'image.jpg', message, m);
 };
 
-handler.customPrefix = /^فلاش$/i;
+handler.customPrefix = /^رزان$/i;
 handler.command = new RegExp;
 
 export default handler;
