1c49d3bf7f2baa96b5bbdf564f12017f043e3766
Ahmad77699900 <bbwth880@gmail.com>
2024-10-13 17:44:11 +0300

    Update gpt-4.js

--- plugins/gpt-4.js
+++ plugins/gpt-4.js
@@ -33,6 +33,6 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
 handler.help = ['gpt4 <text>'];
 handler.tags = ['tools'];
-handler.command = /^(بوت)$/i;
+handler.command = /^(رزان)$/i;
 
 export default handler;
