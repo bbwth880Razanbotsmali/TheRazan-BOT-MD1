99121e7c1630b2904c8225e4ae9a1b29776b2e00
bbwth880Razanbotsmali <razanbots@gmail.com>
2024-10-23 21:04:04 +0300

    Update رد3.js

--- plugins/رد3.js
+++ plugins/رد3.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
     let message = `*قــلــب رزان مـن جـوا 👀⚡*`;
 await conn.sendMessage(m.chat, { react: { text: '🙌', key: m.key } })
-    conn.sendFile(m.chat, 'https://qu.ax/rNkWf.jpg', 'image.jpg', message, m);
+    conn.sendFile(m.chat, 'https://qu.ax/pCuyN.jpg', 'image.jpg', message, m);
 };
 
 handler.customPrefix = /^رزان$/i;
