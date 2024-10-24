866f3a9229494d1865ab2c9a83fe8de063481db9
bbwth880Razanbotsmali <razanbots@gmail.com>
2024-10-24 10:20:25 +0300

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -4,16 +4,22 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `❐ رسالة : ${pesan}`
-let teks = `*┌───⊷﹝المنشن⊰⚜⊱الجماعي﹞+⊷*\n❏ جروب : *${groupMetadata.subject}*\n${oi}\n❏ الاعضاء\n`
+let oi = `✨🌟❐ 𝑅𝑆𝐴𝐿𝐴𝑇𝐾 : ${pesan} 🌟✨`
+let teks = `*🌟🌟🌟┌───⊷﹝المنشن⊰⚜⊱الجماعي﹞+⊷🌟🌟🌟*\n`
+teks += `🎀❏ جروب : *${groupMetadata.subject}* 🎀\n`
+teks += `💫${oi}💫\n`
+teks += `❏ 👥 الاعضاء 👥\n`
+
 for (let mem of participants) {
-teks += `⚡┃⊹ @${mem.id.split('@')[0]}\n`}
-teks += `*𝑅𝐴𝑍𝐴𝑁﹝⚡﹞𝐵𝑂َ𝑇*\n`
-teks += `*└──────────────────⊷*`
+teks += `⚡┃⊹ @${mem.id.split('@')[0]} ⚡\n`
+}
+
+teks += `🎇*𝑅𝐴𝑍𝐴𝑁﹝⚡﹞𝐵𝑂َ𝑇*🎇\n`
+teks += `*🌠└──────────────────⊷🌠*`
+
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )  
 }
 handler.command = /^(tagall|منشن|invocacion|todos|invocación)$/i
 handler.admin = true
 handler.group = true
 export default handler
-
